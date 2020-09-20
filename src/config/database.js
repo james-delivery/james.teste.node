31dd2fe3dcac1f1277d7ac7d23961aff81a38af2
Adriano Terruel <adrianopons@gmail.com>
2020-09-20 09:30:49 -0300

    chore: add estrutura para testes iniciais

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'docker',
+  password: 'teste.james123!',
+  database: 'testeJames',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
