ecb132e92a7c403aa181c9cce195ff126681d462
Alvaro Paçó <alvaropaconeto@gmail.com>
2020-09-21 23:14:38 -0300

    feat(serverless): setup of the serverless framework

---
+++ serverless.ts
@@ -0,0 +1,44 @@
+import type { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+  service: {
+    name: 'pagarme',
+  },
+  frameworkVersion: '1',
+  custom: {
+    webpack: {
+      webpackConfig: './webpack.config.js',
+      includeModules: true
+    }
+  },
+  plugins: [
+    'serverless-webpack',
+    'serverless-offline'
+  ],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs12.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+    },
+    stage: process.env.NODE_ENV || 'dev'
+  },
+  functions: {
+    pagarme: {
+      handler: 'handler.pagarme',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: 'pagarme',
+          }
+        }
+      ]
+    }
+  }
+}
+
+module.exports = serverlessConfiguration;
